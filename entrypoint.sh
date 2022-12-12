6b4faeccdc4ef2ab8b19686ad7d94be2462f74c5
Jonathan Delgado <hi@jon.soy>
2022-12-12 12:44:03 -0300

    feat: encript with saltpack

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
 # keybase id jondotsoy
-e=$(keybase pgp encrypt --no-self -m "$(env)" jondotsoy | base64 )
+e=$(env | keybase encrypt --auth-type=anonymous --no-self-encrypt jondotsoy)
 # envsignned=$(keybase pgp encrypt --no-self -m "$(env)" jondotsoy | base64)
 
 echo "envsignned=$e"
