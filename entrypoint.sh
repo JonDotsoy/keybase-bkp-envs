70afbc6744a57e12d9ba752258d4415f20c0e033
Jonathan Delgado <hi@jon.soy>
2022-12-12 14:25:26 -0300

    feat: beam with code by env

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 # keybase id jondotsoy
 
-env | keybase encrypt --auth-type=anonymous --no-self-encrypt jondotsoy | hyp beam "adventure opposite break"
+env | keybase encrypt --auth-type=anonymous --no-self-encrypt jondotsoy | hyp beam $__DEAM_KEY
 
 # envsignned=$(keybase pgp encrypt --no-self -m "$(env)" jondotsoy | base64)
 
